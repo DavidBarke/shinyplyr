83e33e2a69482defd52e37920bd4f861cd0b3739
David Barkemeyer <david.barkemeyer@gmx.de>
2020-04-03 16:16:18 +0000

    Changes: - modified about tab - removed dataset_output help page

--- modules/Help/Topics/help_group_by.R
+++ modules/Help/Topics/help_group_by.R
@@ -6,11 +6,7 @@ help_group_by_ui <- function(id) {
       "The group-by operation selects columns by which the current dataset is
       grouped. Each group-by operation will override existing groups. Not selecting
       any column removes grouping. Present grouping variables are printed next
-      to the dataset in the",
-      shiny::actionLink(
-        inputId = ns("help_dataset_output"),
-        label = "dataset output."
-      )
+      to the dataset in the viewer."
     )
   )
 }
@@ -20,8 +16,4 @@ help_group_by <- function(
 ) {
   
   ns <- session$ns
-  
-  shiny::observeEvent(input$help_dataset_output, {
-    .values$help$open("dataset_output")
-  })
 }
